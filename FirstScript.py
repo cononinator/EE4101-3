5b60cd1b1631b8ffe799387e0a5ef91ec87739ea
cononinator <80430573+cononinator@users.noreply.github.com>
2024-11-13 11:18:43 +0000

    Initial commit

--- FirstScript.py
+++ FirstScript.py
@@ -57,7 +57,7 @@ X = concrete_compressive_strength.data.features
 y = concrete_compressive_strength.data.targets
 
 # Split the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 # Scale the features
 scaler = StandardScaler()
